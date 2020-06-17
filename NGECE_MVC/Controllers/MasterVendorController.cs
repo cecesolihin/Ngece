bc0411909a0ab92efa3bea2b8028c5bacc032c9a
cece_solihin <solihincece0@gmail.com>
2020-06-17 10:41:36 +0700

    comit baru

---
+++ NGECE_MVC/Controllers/MasterVendorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NGECE_MVC.Controllers
+{
+    public class MasterVendorController : Controller
+    {
+        //
+        // GET: /MasterVendor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
